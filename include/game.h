1611afbf67225860f2a41682fb325ffdd514d78b
hossainm10 <hossain.mahatheer@uconn.edu>
2026-06-01 16:36:17 -0400

    Header files completed

--- include/game.h
+++ include/game.h
@@ -1,1 +1,20 @@
 #pragma once
+
+enum Direction{ UP,DOWN, LEFT,RIGHT};
+
+class Game{
+    public:
+        Game();
+        void reset();
+        bool move(Direction dir);
+        bool isOver() const;
+        bool hasWon() const;
+        int getScore() const;
+        const int(*getBoard() const)[4];
+    private:
+        int board[4][4];
+        int score;
+        void spawnTile();
+        bool canMove() const;
+        bool slideRow(int row[4]);
+};
--- include/renderer.h
+++ include/renderer.h
@@ -1,1 +1,22 @@
 #pragma once
+#include "game.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+class Renderer{
+    public:
+        Renderer(int windowW, int windowH);
+        ~Renderer();
+
+        void draw(const Game& game);
+        void drawBoard(const int board[4][4]);
+        void drawTile(int value, int x, int y);
+        void drawScore(int score);
+        void drawGameOver();
+        void present();
+    private:
+        SDL_Window* window;
+        SDL_Renderer* renderer;
+        TTF_Font* font;
+        SDL_Color tileColor(int value);
+};
